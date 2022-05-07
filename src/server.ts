33ab76292da86949d8de6993fabe67008647d144
Leonel <leo@tobs.com.ar>
2022-05-07 15:40:07 -0300

    add default port

--- src/server.ts
+++ src/server.ts
@@ -37,8 +37,8 @@ class Server {
    }
 
    start() {
-      this.app.listen(process.env.PORT, () => {
-         console.log("Server on port", process.env.PORT);
+      this.app.listen(process.env.PORT || 3000, () => {
+         console.log("Server on port", process.env.PORT || 3000);
       });
    }
 
